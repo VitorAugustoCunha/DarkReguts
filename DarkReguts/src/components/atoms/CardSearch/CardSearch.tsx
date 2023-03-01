9444c9245c39d9fbe8a8fbc9dc7ba6cc4c8d0817
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2023-03-01 00:52:53 -0300

    CardSearch

--- DarkReguts/src/components/atoms/CardSearch/CardSearch.tsx
+++ DarkReguts/src/components/atoms/CardSearch/CardSearch.tsx
@@ -1,4 +1,6 @@
 import styles from './CardSearch.module.scss'
+import PopularIcon from '../../../assets/PopularIcon'
+import { Link } from "react-router-dom";
 
 interface CardSearchProps {
     title: string
@@ -8,13 +10,35 @@ interface CardSearchProps {
     imagem?: string
 }
 
-export const CardSearch = ({title, content, page, pageIcon, imagem}: CardSearchProps) => {
+const CardSearch = ({title, content, page, pageIcon, imagem}: CardSearchProps) => {
     return(
-        <div className={styles.card}>
-            <h3>{title}</h3>
-            <p>{content}</p>
-            <img src={imagem} alt={title}/>
-            <p><span>{pageIcon}</span> d/{page}</p>
-        </div>
+            <div className={styles.card}>
+                <div className={styles.top}>
+                    <div className={styles.icon}>
+                        <PopularIcon/>
+                    </div>
+                <div className={styles.title}>
+                    {title}
+                </div>
+            </div>
+            <div className={styles.middle}>
+                <div className={styles.content}>
+                    {content}
+                </div>
+                <div className={styles.image}>
+                    <img src={imagem} alt={title}/>
+                </div>
+            </div>
+            <div className={styles.bottom}>
+                <div className={styles.page__icon}>
+                    <img src={pageIcon} alt={title} />
+                </div>
+                    <div className={styles.page}>
+                        d/{page}
+                    </div>
+                </div>
+            </div>
     );
 }
+
+export default CardSearch
