16a1eba4d06c1c46d3c936f45989977fa5f91532
Vitor Augusto da Cunha <103552917+VitorAugustoCunha@users.noreply.github.com>
2023-03-01 10:01:17 -0300

    Começando feed buttons

--- DarkReguts/src/App.tsx
+++ DarkReguts/src/App.tsx
@@ -1,15 +1,12 @@
 import './App.css'
-import Button from './components/atoms/Button/Button'
 import { CardSearch } from './components/atoms/CardSearch/CardSearch'
 import Search from './components/atoms/Search/Search'
-
 export const App = () => {
-
   return (
     <div className="App">
       dasdasdasdasdadsadasdasd
       <Search/>
-      <Button > Clica ai po</Button>
+      <CardSearch title="Vitor test" content='Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique inventore dolorum, non incidunt et, omnis iusto explicabo fugit magnam perferendis qui. Eveniet, cupiditate. Voluptatibus nesciunt eos totam voluptatum dignissimos sed?' imagem='https://wl-incrivel.cf.tsp.li/resize/728x/jpg/91b/430/964a9c5ac9933cc012d0bd80be.jpg' pageIcon='https://wl-incrivel.cf.tsp.li/resize/728x/jpg/91b/430/964a9c5ac9933cc012d0bd80be.jpg' page='Vitor'/>
     </div>
   )
 }
